3b5f98d50217e167ba8345427ff6008a1d030624
Dysio <td.dyszewski@gmail.com>
2021-04-29 21:24:57 +0200

    Added split function
    Added split function in main.py and updated split.html, layout.html

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import os
 from flask import Flask, redirect, render_template, request, session, \
     send_file, send_from_directory, safe_join, abort
 
-from PyPDF2 import PdfFileMerger
+from pathlib import Path
+from PyPDF2 import PdfFileMerger, PdfFileReader, PdfFileWriter
 import werkzeug
 
 from helpers import directory_check, files_delete
@@ -31,35 +32,37 @@ def allowed_extensions(filename):
     else:
         return False
 
+
 def files_prepare():
 
-        directory_check(dir_path_uploads)
-        directory_check(dir_path_downloads)
-        files_delete(dir_path_uploads)
-        files_delete(dir_path_downloads)
+    directory_check(dir_path_uploads)
+    directory_check(dir_path_downloads)
+    files_delete(dir_path_uploads)
+    files_delete(dir_path_downloads)
 
-        uploaded_pdfs = request.files.getlist("pdf")
-        print(f"Pdfs: {uploaded_pdfs}")
+    uploaded_pdfs = request.files.getlist("pdf")
+    print(f"Pdfs: {uploaded_pdfs}")
 
-        for pdf in uploaded_pdfs:
-            if pdf.filename == '':
-                print('No selected file')
-                return redirect(request.url)
+    for pdf in uploaded_pdfs:
+        if pdf.filename == '':
+            print('No selected file')
+            return redirect(request.url)
 
-            if allowed_extensions(pdf.filename):
-                filename = werkzeug.utils.secure_filename(pdf.filename)
+        if allowed_extensions(pdf.filename):
+            filename = werkzeug.utils.secure_filename(pdf.filename)
 
-                pdf.save(os.path.join(app.config['PDF_UPLOADS'], filename))
-                print(f"pdf {filename} saved")
+            pdf.save(os.path.join(app.config['PDF_UPLOADS'], filename))
+            print(f"pdf {filename} saved")
 
-            else:
-                print("That file extension is not allowed")
-                return redirect(request.url)
+        else:
+            print("That file extension is not allowed")
+            return redirect(request.url)
 
 
 @app.route("/")
 def index():
     return render_template('index.html')
+
 
 @app.route("/download")
 def download_file():
@@ -68,6 +71,7 @@ def download_file():
                                    cache_timeout=0)
     except FileNotFoundError:
         abort(404)
+
 
 @app.route("/merge", methods=["GET", "POST"])
 def merge():
@@ -94,16 +98,56 @@ def merge():
 
     return render_template('merge.html')
 
-@app.route("/split")
+
+@app.route("/split", methods=['GET', 'POST'])
 def split():
+    if request.method == "POST":
+        files_prepare()
+
+        pages = request.form.get("pages")
+        print(f'pages: {pages}')
+
+        path = os.path.join(os.getcwd(), 'static\pdf_uploads')
+        input_pdf = PdfFileReader(str(os.path.join(dir_path_uploads, os.listdir(dir_path_uploads)[0])))
+        pdf_writer = PdfFileWriter()
+        iterator = 0
+        num_of_pages = input_pdf.getNumPages()
+        save_path = os.path.join(dir_path_downloads)
+        page_split = [int(i) for i in pages.split(',')]
+        print(page_split)
+
+        if page_split[-1] != num_of_pages:
+            page_split.append(num_of_pages)
+
+        for num in page_split:
+            if type(num) is not int:
+                raise ValueError("You have to pass positive integer")
+            if num > num_of_pages:
+                raise ValueError("Number out of range")
+            if num < 0:
+                raise ValueError("Number must be positive integer")
+
+            for iterator in range(iterator, num_of_pages):
+                page = input_pdf.getPage(iterator)
+                pdf_writer.addPage(page)
+                iterator += 1
+                if iterator == num:
+                    filename = f'Result{num}.pdf'
+                    with Path(os.path.join(save_path), filename).open(mode="wb") as output_file:
+                        pdf_writer.write(output_file)
+                    pdf_writer = PdfFileWriter()
+                    break
+
+        return redirect(request.url)
 
     return render_template('split.html')
 
-@app.route("/split_size")
+
+@app.route("/split_size", methods=["GET", "POST"])
 def split_size():
     return render_template('split_size.html')
 
-@app.route("/rotate")
+@app.route("/rotate", methods=["GET", "POST"])
 def rotate():
     return render_template('rotate.html')
 
